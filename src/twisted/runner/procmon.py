3b36166f7310560895b39df029f7c89ef87226a4
Glyph <code@glyph.im>
2025-02-26 13:57:17 -0800

    complete type coverage and removal of exception while I'm here

--- src/twisted/runner/procmon.py
+++ src/twisted/runner/procmon.py
@@ -7,13 +7,19 @@ Support for starting, monitoring, and restarting child process.
 """
 from __future__ import annotations
 
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Optional
 
 import attr
 import incremental
 
 from twisted.application import service
 from twisted.internet import error, protocol, reactor as _reactor
+from twisted.internet.interfaces import (
+    IDelayedCall,
+    IProcessTransport,
+    IReactorProcess,
+    IReactorTime,
+)
 from twisted.logger import Logger
 from twisted.protocols import basic
 from twisted.python import deprecate
@@ -49,7 +55,7 @@ class _Process:
     cwd: Optional[str] = None
 
     @deprecate.deprecated(incremental.Version("Twisted", 18, 7, 0))
-    def toTuple(self):
+    def toTuple(self) -> tuple[list[str], int | None, int | None, dict[str, str]]:
         """
         Convert process to tuple.
 
@@ -64,8 +70,6 @@ class _Process:
 
         This allows changing the internal structure of the process list,
         when warranted by bug fixes or additional features.
-
-        @return: tuple representation of process
         """
         return (self.args, self.uid, self.gid, self.env)
 
@@ -90,14 +94,14 @@ class LineLogger(basic.LineReceiver):
     delimiter = b"\n"
     service = None
 
-    def lineReceived(self, line):
+    def lineReceived(self, line: bytes) -> None:
         try:
-            line = line.decode("utf-8")
+            sline = line.decode("utf-8")
         except UnicodeDecodeError:
-            line = repr(line)
+            sline = repr(line)
 
         self.service.log.info(
-            "[{tag}] {line}", tag=self.tag, line=line, stream=self.stream
+            "[{tag}] {line}", tag=self.tag, line=sline, stream=self.stream
         )
 
 
@@ -126,11 +130,11 @@ class LoggingProtocol(protocol.ProcessProtocol):
         self._output.makeConnection(transport)
         self._error.makeConnection(transport)
 
-    def outReceived(self, data):
+    def outReceived(self, data: bytes) -> None:
         self._output.dataReceived(data)
         self._outputEmpty = data[-1] == b"\n"
 
-    def errReceived(self, data):
+    def errReceived(self, data: bytes) -> None:
         self._error.dataReceived(data)
         self._errorEmpty = data[-1] == b"\n"
 
@@ -142,40 +146,40 @@ class LoggingProtocol(protocol.ProcessProtocol):
         self.service._monitoredProcessExited(self.name, reason)
 
     @property
-    def output(self):
+    def output(self) -> LineLogger:
         return self._output
 
     @property
-    def empty(self):
+    def empty(self) -> bool:
         return self._outputEmpty
 
 
 class ProcessMonitor(service.Service):
     """
-    ProcessMonitor runs processes, monitors their progress, and restarts
-    them when they die.
+    ProcessMonitor runs processes, monitors their progress, and restarts them
+    when they die.
 
     The ProcessMonitor will not attempt to restart a process that appears to
     die instantly -- with each "instant" death (less than 1 second, by
-    default), it will delay approximately twice as long before restarting
-    it.  A successful run will reset the counter.
+    default), it will delay approximately twice as long before restarting it.
+    A successful run will reset the counter.
 
-    The primary interface is L{addProcess} and L{removeProcess}. When the
+    The primary interface is L{addProcess} and L{removeProcess}.  When the
     service is running (that is, when the application it is attached to is
     running), adding a process automatically starts it.
 
-    Each process has a name. This name string must uniquely identify the
-    process.  In particular, attempting to add two processes with the same
-    name will result in a C{KeyError}.
+    Each process has a name.  This name string must uniquely identify the
+    process.  In particular, attempting to add two processes with the same name
+    will result in a C{KeyError}.
 
     @type threshold: C{float}
     @ivar threshold: How long a process has to live before the death is
         considered instant, in seconds.  The default value is 1 second.
 
     @type killTime: C{float}
-    @ivar killTime: How long a process being killed has to get its affairs
-        in order before it gets killed with an unmaskable signal.  The
-        default value is 5 seconds.
+    @ivar killTime: How long a process being killed has to get its affairs in
+        order before it gets killed with an unmaskable signal.  The default
+        value is 5 seconds.
 
     @type minRestartDelay: C{float}
     @ivar minRestartDelay: The minimum time (in seconds) to wait before
@@ -186,13 +190,12 @@ class ProcessMonitor(service.Service):
         attempting to restart a process.  Default 3600s (1h).
 
     @type _reactor: L{IReactorProcess} provider
-    @ivar _reactor: A provider of L{IReactorProcess} and L{IReactorTime}
-        which will be used to spawn processes and register delayed calls.
+    @ivar _reactor: A provider of L{IReactorProcess} and L{IReactorTime} which
+        will be used to spawn processes and register delayed calls.
 
     @type log: L{Logger}
     @ivar log: The logger used to propagate log messages from spawned
         processes.
-
     """
 
     threshold = 1
@@ -201,18 +204,24 @@ class ProcessMonitor(service.Service):
     maxRestartDelay = 3600
     log = Logger()
 
-    def __init__(self, reactor=_reactor):
+    def __init__(
+        self,
+        reactor: IReactorProcess = _reactor,  # type:ignore
+    ) -> None:
         self._reactor = reactor
+        self._clock = IReactorTime(reactor)
 
-        self._processes = {}
-        self.protocols = {}
-        self.delay = {}
-        self.timeStarted = {}
-        self.murder = {}
-        self.restart = {}
+        self._processes: dict[str, _Process] = {}
+        self.protocols: dict[str, LoggingProtocol] = {}
+        self.delay: dict[str, float] = {}
+        self.timeStarted: dict[str, float] = {}
+        self.murder: dict[str, IDelayedCall] = {}
+        self.restart: dict[str, IDelayedCall] = {}
 
     @deprecate.deprecatedProperty(incremental.Version("Twisted", 18, 7, 0))
-    def processes(self):
+    def processes(
+        self,
+    ) -> dict[str, tuple[list[str], int | None, int | None, dict[str, str]]]:
         """
         Processes as dict of tuples
 
@@ -221,7 +230,7 @@ class ProcessMonitor(service.Service):
         return {name: process.toTuple() for name, process in self._processes.items()}
 
     @deprecate.deprecated(incremental.Version("Twisted", 18, 7, 0))
-    def __getstate__(self):
+    def __getstate__(self) -> Any:
         dct = service.Service.__getstate__(self)
         del dct["_reactor"]
         dct["protocols"] = {}
@@ -233,32 +242,41 @@ class ProcessMonitor(service.Service):
         dct["processes"] = self.processes
         return dct
 
-    def addProcess(self, name, args, uid=None, gid=None, env={}, cwd=None):
+    def addProcess(
+        self,
+        name: str,
+        args: list[str],
+        uid: int | None = None,
+        gid: int | None = None,
+        env: dict[str, str] = {},
+        cwd: str | None = None,
+    ) -> None:
         """
         Add a new monitored process and start it immediately if the
         L{ProcessMonitor} service is running.
 
-        Note that args are passed to the system call, not to the shell. If
+        Note that args are passed to the system call, not to the shell.  If
         running the shell is desired, the common idiom is to use
         C{ProcessMonitor.addProcess("name", ['/bin/sh', '-c', shell_script])}
 
-        @param name: A name for this process.  This value must be
-            unique across all processes added to this monitor.
-        @type name: C{str}
+        @param name: A name for this process.  This value must be unique across
+            all processes added to this monitor.
+
         @param args: The argv sequence for the process to launch.
-        @param uid: The user ID to use to run the process.  If L{None},
-            the current UID is used.
-        @type uid: C{int}
-        @param gid: The group ID to use to run the process.  If L{None},
-            the current GID is used.
-        @type uid: C{int}
-        @param env: The environment to give to the launched process. See
+
+        @param uid: The user ID to use to run the process.  If L{None}, the
+            current UID is used.
+
+        @param gid: The group ID to use to run the process.  If L{None}, the
+            current GID is used.
+
+        @param env: The environment to give to the launched process.  See
             L{IReactorProcess.spawnProcess}'s C{env} parameter.
-        @type env: C{dict}
-        @param cwd: The initial working directory of the launched process.
-            The default of C{None} means inheriting the laucnhing process's
-            working directory.
-        @type env: C{dict}
+
+        @param cwd: The initial working directory of the launched process.  The
+            default of C{None} means inheriting the laucnhing process's working
+            directory.
+
         @raise KeyError: If a process with the given name already exists.
         """
         if name in self._processes:
@@ -268,18 +286,17 @@ class ProcessMonitor(service.Service):
         if self.running:
             self.startProcess(name)
 
-    def removeProcess(self, name):
+    def removeProcess(self, name: str) -> None:
         """
         Stop the named process and remove it from the list of monitored
         processes.
 
-        @type name: C{str}
         @param name: A string that uniquely identifies the process.
         """
         self.stopProcess(name)
         del self._processes[name]
 
-    def startService(self):
+    def startService(self) -> None:
         """
         Start all monitored processes.
         """
@@ -287,7 +304,7 @@ class ProcessMonitor(service.Service):
         for name in list(self._processes):
             self.startProcess(name)
 
-    def stopService(self):
+    def stopService(self) -> None:
         """
         Stop all monitored processes and cancel all scheduled process restarts.
         """
@@ -348,7 +365,7 @@ class ProcessMonitor(service.Service):
 
         del self.protocols[name]
 
-        if self._reactor.seconds() - self.timeStarted[name] < self.threshold:
+        if self._clock.seconds() - self.timeStarted[name] < self.threshold:
             # The process died too fast - backoff
             nextDelay = self.delay[name]
             self.delay[name] = min(self.delay[name] * 2, self.maxRestartDelay)
@@ -361,11 +378,11 @@ class ProcessMonitor(service.Service):
 
         # Schedule a process restart if the service is running
         if self.running and name in self._processes:
-            self.restart[name] = self._reactor.callLater(
+            self.restart[name] = self._clock.callLater(
                 nextDelay, self.startProcess, name
             )
 
-    def startProcess(self, name):
+    def startProcess(self, name: str) -> None:
         """
         @param name: The name of the process to be started
         """
@@ -380,7 +397,7 @@ class ProcessMonitor(service.Service):
         proto.service = self
         proto.name = name
         self.protocols[name] = proto
-        self.timeStarted[name] = self._reactor.seconds()
+        self.timeStarted[name] = self._clock.seconds()
         try:
             self._reactor.spawnProcess(
                 proto,
@@ -392,18 +409,15 @@ class ProcessMonitor(service.Service):
                 path=process.cwd,
             )
         except OSError:
-            self.processExit(name, Failure())
+            self._monitoredProcessExited(name, Failure())
 
-    def _forceStopProcess(self, proc):
-        """
-        @param proc: An L{IProcessTransport} provider
-        """
+    def _forceStopProcess(self, proc: IProcessTransport) -> None:
         try:
             proc.signalProcess("KILL")
         except error.ProcessExitedAlready:
             pass
 
-    def stopProcess(self, name):
+    def stopProcess(self, name: str) -> None:
         """
         @param name: The name of the process to be stopped
         """
@@ -413,16 +427,17 @@ class ProcessMonitor(service.Service):
         proto = self.protocols.get(name, None)
         if proto is not None:
             proc = proto.transport
+            assert proc is not None
             try:
                 proc.signalProcess("TERM")
             except error.ProcessExitedAlready:
                 pass
             else:
-                self.murder[name] = self._reactor.callLater(
+                self.murder[name] = self._clock.callLater(
                     self.killTime, self._forceStopProcess, proc
                 )
 
-    def restartAll(self):
+    def restartAll(self) -> None:
         """
         Restart all processes. This is useful for third party management
         services to allow a user to restart servers because of an outside change
